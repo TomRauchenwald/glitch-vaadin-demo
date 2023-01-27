a0c63b4fb48140a95c0cbdf2dd79042e169821c5
Tom Rauchenwald <tom.rauchenwald@unifits.com>
2023-01-27 14:20:45 +0100

    Initial

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [],
+    resolve: {
+        alias: {
+            find: '@/',
+            replacement: path.resolve(__dirname, './src')
+        }
+    },
+    build: {
+        outDir: "build"
+    },
+    server: {
+        strictPort: true,
+    }
+})
